3290c1229003462b5f57136f98125c3ddab97a74
Landon <landoncramer1@gmail.com>
2023-07-11 14:56:33 -0700

    dom modify elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+ const element = document.querySelector('main#main');
+ element.remove();
+
+ const newHeader = document.createElement('h1');
+ 
+
+  newHeader.id = 'victory';
+
+  const yourName = 'YOUR-NAME';
+  newHeader.textContent =  yourName +" is the champion";
+
+  document.body.appendChild(newHeader);
